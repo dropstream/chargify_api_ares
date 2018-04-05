1109ac088b786f0b49596a1aeec5713e7f9b53f6
Wojtek Kruszewski <wojtek@kruszewski.com>
2018-04-05 09:48:07 -0400

    Add PricePoint resource

--- lib/chargify_api_ares/resources/subscription.rb
+++ lib/chargify_api_ares/resources/subscription.rb
@@ -200,6 +200,17 @@ module Chargify
       def id
         self.component_id
       end
+
+      def price_points(params = {})
+        params.merge!({:component_id => self.id})
+        ::Chargify::Subscription::Component::PricePoint.find(:all, :params => params)
+      end
+
+      private
+
+      class PricePoint < Base
+        self.prefix = "/components/:component_id/"
+      end
     end
 
     class Event < Base
